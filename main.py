34644567023e9a986ba1e20687eca53aff3f17b1
Akimov Anton <akimov738@gmail.com>
2024-08-31 00:53:58 +0300

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+#https://yar.stararbat.ru/
+#https://yandex.ru/pogoda/yaroslavl?via=reg&lat=57.541603&lon=39.975708
+url = "https://dikidi.net/687868"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text,"html.parser")
+print(soup)
+print(page.status_code)
