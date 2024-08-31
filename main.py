576452c76f6fa58448ca05d00b31bc5ad8b21489
Akimov Anton <akimov738@gmail.com>
2024-08-31 21:40:11 +0300

    test commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
+# commit test
 #https://yar.stararbat.ru/
 #https://yandex.ru/pogoda/yaroslavl?via=reg&lat=57.541603&lon=39.975708
 url = "https://dikidi.net/687868"
