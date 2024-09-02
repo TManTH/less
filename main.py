122973a7477ca493b79e3086c26b8b98af359ff7
Akimov Anton <akimov738@gmail.com>
2024-09-02 07:00:42 +0300

    main.py edit

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-
-# commit test
+#https://apteka.ru/
+# https://apteka.ru/search/?q=пенталгин  запрос поиска
 #https://yar.stararbat.ru/
 #https://yandex.ru/pogoda/yaroslavl?via=reg&lat=57.541603&lon=39.975708
-url = "https://dikidi.net/687868"
-page = requests.get(url)
 
-soup = BeautifulSoup(page.text,"html.parser")
-print(soup)
+url_list = ["https://apteka.ru/"," "]
+print(url_list)
+page = requests.get(url_list[0]+"search/?q=пенталгин")
 print(page.status_code)
+soup = BeautifulSoup(page.text,"html.parser")
+
+print(soup)
